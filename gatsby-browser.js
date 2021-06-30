b25472b80d77547e1d51763db7e41f7907fbd028
Javier López Úbeda <jlopezcur@gmail.com>
2021-06-30 23:29:50 +0200

    fix: vh on mobile
    second part
    close #1

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,12 @@ import './src/styles/reset.scss';
 // Mobile workaround for vh
 // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 if (window) {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  const updateVH = () => {
+    const vh = window.innerHeight / 100;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+  window.addEventListener('resize', updateVH);
+  updateVH();
 }
 
 export const wrapPageElement = ({ element }) => {
